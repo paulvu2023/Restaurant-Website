c5a961f9570e3939e89afb09ec19f841bf3a9bc6
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-08-28 12:26:22 -0700

    Add working tabbed browsing

--- src/navbar.js
+++ src/navbar.js
@@ -30,24 +30,35 @@ const loadNavbar = () => {
     selectedNavbar.appendChild(aboutButton);
 
     homeButton.addEventListener('click', () => {
-        clearDOM();
+        clearContent();
         loadHomePage();
     });
 
     menuButton.addEventListener('click', () => {
-        clearDOM();
+        clearContent();
         loadMenuPage();
     });
 
     aboutButton.addEventListener('click', () => {
-        clearDOM();
+        clearContent();
         loadAboutPage();
     });
 };
 
-function clearDOM (){
-    const content = document.querySelector('#content');
-    content.textContent = '';
+function clearContent(){
+    const home_container = document.querySelector('.home_container');
+    const about_container = document.querySelector('.about_container');
+    const menu_container = document.querySelector('.menu_container');
+
+    if (home_container) {
+        home_container.remove();
+    }
+    if (menu_container) {
+        menu_container.remove();
+    }
+    if (about_container) {
+        about_container.remove();
+    }
 }
 
 export default loadNavbar;
